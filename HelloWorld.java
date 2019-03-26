39aeb93d5b9fba6f164d182389a08911bfc54f39
miggycaloy <48932591+miggycaloy@users.noreply.github.com>
2019-03-26 08:44:51 +0800

    Update HelloWorld.java
    Added comments here lamaster

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I've been here!");
+        System.out.println("Miguel Carlo C. Vito");
     }
 }
